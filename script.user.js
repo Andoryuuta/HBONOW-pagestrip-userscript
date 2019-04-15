038acbb3ab6c1c3f9cb03a03e6e2967ee049b8dd
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2019-04-14 20:47:09 -0700

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -13,27 +13,46 @@
 (function() {
     'use strict';
 
+    var stripModeActive = false;
+
+    var resizeTimer;
+    $(window).on('resize', function(e) {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function() {
+            if(stripModeActive){
+                reset_styles();
+            }
+        }, 50);
+    });
+
     document.addEventListener("keydown", function (e) {
         if (e.ctrlKey && e.altKey && e.key === "s"){
-            strip_page();
+            stripModeActive = !stripModeActive;
+            if(stripModeActive){
+                strip_page();
+            }
         }
     });
 
     function strip_page() {
-        try {
+        try{
             var body = document.body;
-            body.style = "align-items: center; display: flex; justify-content: center; height: 100%; width: 100%;";
-
             var vid = document.querySelector("video");
             vid.setAttribute('controls', true);
             body.insertBefore(vid, body.firstChild);
-            vid.style = "width: 100%; height: 100%;"
-
+            reset_styles();
 
             $("video").nextAll().remove()
-        }
-        catch(err) {
-            console.log(err.message);
+        }catch(e){
+            alert("Video must be playing first");
+            stripModeActive = false;
         }
     }
+
+    function reset_styles() {
+        document.querySelector("video").style = "width: 100%; height: 100%;";
+        document.body.style = "align-items: center; display: flex; justify-content: center; height: 100%; width: 100%;";
+
+    }
+
 })();
